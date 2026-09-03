f08c973cfe9c50195af9da389262d0071f699808
Souvik Nag <s.nag26@gmail.com>
2026-09-03 15:25:35 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+1. npm i babel-plugin-react-compiler - important for memoizing data and prevent re-render
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <>
+      <div className="font-manrope text-3xl">This is a Heading</div>
+      <div className="font-inter">Welcome to NextJS</div>
+    </>
+  );
+};
+export default Home;
