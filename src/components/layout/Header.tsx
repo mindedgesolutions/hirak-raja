435f640e43d573149a80f21314b5a1294b9e732b
Souvik Nag <s.nag26@gmail.com>
2026-09-03 17:27:20 +0530

    constants > index created

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,28 @@
+import { webIcons } from '@/constants';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="bg-primary font-inter">
+      <div className="max-w-5xl flex justify-between items-center py-2">
+        <div className="flex gap-4">
+          <div className="flex items-center gap-1.5 text-card">
+            <webIcons.location size={12} />
+            <span className="text-xs tracking-wider">
+              123, Lorem ipsum dolor sit amet
+            </span>
+          </div>
+          <div className="flex items-center gap-1.5 text-card">
+            <webIcons.phone size={12} />
+            <span className="text-xs tracking-wider">+91-1234567890</span>
+          </div>
+          <div className="flex items-center gap-1.5 text-card">
+            <webIcons.email size={14} />
+            <span className="text-xs tracking-wider">test@test.com</span>
+          </div>
+        </div>
+        <div className=""></div>
+      </div>
+    </div>
+  );
 };
 export default Header;
---
+++ src/constants/index.tsx
@@ -0,0 +1,8 @@
+import { FaLocationDot, FaPhone } from 'react-icons/fa6';
+import { IoMdMail } from 'react-icons/io';
+
+export const webIcons = {
+  location: FaLocationDot,
+  phone: FaPhone,
+  email: IoMdMail,
+};
